ad311f4f40cea2179375c180ef71ce50740a2809
Mighty Ayanbamiji <mightytomisin@gmail.com>
2025-08-05 11:05:40 +0100

    updated shopping list file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -2,7 +2,7 @@ def display_menu():
     """
     Displays the menu options to the user.
     """
-    print("\n--- Shopping List Manager ---")
+    print("Shopping List Manager")
     print("1. Add item")
     print("2. Remove item")
     print("3. View list")
