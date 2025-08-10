3bd4a634dfc5809a152586eca022861ca3dd313e
Mighty Ayanbamiji <mightytomisin@gmail.com>
2025-08-10 12:34:21 +0100

    uploaded the calculator file

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,50 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    """
+    A test suite for the SimpleCalculator class.
+    """
+
+    def setUp(self):
+        """
+        Set up a new SimpleCalculator instance before each test method.
+        """
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        """Test the add method with various inputs."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -5), -10)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        """Test the subtract method with various inputs."""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-5, -2), -3)
+        self.assertEqual(self.calc.subtract(10, 0), 10)
+
+    def test_multiply(self):
+        """Test the multiply method with various inputs."""
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(-5, -5), 25)
+        self.assertEqual(self.calc.multiply(10, 0), 0)
+
+    def test_divide(self):
+        """Test the divide method for normal operation."""
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(10, -2), -5)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+
+    def test_divide_by_zero(self):
+        """Test the divide method for the ZeroDivisionError edge case."""
+        # Use assertRaises to check that the correct exception is raised
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(10, 0)
+
+# This block allows the script to be run directly to execute the tests.
+if __name__ == '__main__':
+    unittest.main()
